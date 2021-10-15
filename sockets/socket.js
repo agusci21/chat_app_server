bfe4e9cf3ca46006bf713b04a51e44ddd1db7d4f
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-10-15 11:33:49 -0300

    Eliminar usuarios no autenticados

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,20 +1,20 @@
-const { io } = require('../index');
-
+const { comprobarJWT } = require('../helpers/jwt')
+const { io } = require('../index')
 
 // Mensajes de Sockets
-io.on('connection', client => {
-    console.log('Cliente conectado');
+io.on('connection', (client) => {
+  console.log('Cliente conectado')
 
-    client.on('disconnect', () => {
-        console.log('Cliente desconectado');
-    });
+  const [valido, uid] = comprobarJWT(client.handshake.headers['x-token'])
 
+  console.log(valido, uid)
 
-    
-    // client.on('mensaje', ( payload ) => {
-    //     console.log('Mensaje', payload);
-    //     io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
-    // });
+  if (!valido) {
+    return client.disconnect()
+  }
 
+  client.on('disconnect', () => {
+    console.log('Cliente desconectado')
+  })
 
-});
+})
